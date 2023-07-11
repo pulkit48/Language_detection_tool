7f52cb1d8947d0fc819e0d640e04f02c3de315e0
pulkit48 <135384814+pulkit48@users.noreply.github.com>
2023-07-11 23:52:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sklearn
+import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
